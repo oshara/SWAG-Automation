0879f85b2d6ba3adba4cafa467e2c84b10e6abc5
Oshara Amarasiriwardena <amanthi1995@gmail.com>
2021-06-10 01:32:46 +0530

    first commit

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,17 @@
+
+
+describe('Login to SWAG_labs',function(){
+it('visit the URL',function(){
+    cy.wait(6000).visit('https://www.saucedemo.com/')
+})
+it('user login to the Application',function(){
+    
+    cy.get('#user-name').type('standard_user')
+    cy.get('#password').type('secret_sauce')
+    cy.get('#login-button').click()
+
+ })
+ it('User add to cart a item',function(){
+     cy.get("#add-to-cart-sauce-labs-backpack").click()
+ })
+})
